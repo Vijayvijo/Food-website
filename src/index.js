53302375c78066565ae119f4f56a2b390db69043
karthik <karthikkarthikstark@gmail.com>
2024-11-11 12:12:43 +0530

    new update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Ensure this file includes global styles
+import './index.css'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from './components/ThemeContext'; // Adjust the path as necessary
-import { AuthProvider } from './components/Login'; // Adjust the path to Login component if necessary
+import { ThemeProvider } from './components/ThemeContext';
+import { AuthProvider } from './components/Login'; 
 
-// Create the root element for rendering
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// Render the application within the ThemeProvider and AuthProvider
 root.render(
   <React.StrictMode>
     <ThemeProvider>
@@ -21,5 +19,5 @@ root.render(
   </React.StrictMode>
 );
 
-// Performance measuring function (optional)
+
 reportWebVitals();
